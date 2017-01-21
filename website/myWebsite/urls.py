8d201f572f5fa0c8c447002f1ad36caa5665720f
LuD1161 <aseemshrey@gmail.com>
2017-01-21 17:37:34 +0530

    Removing Linter

--- website/myWebsite/urls.py
+++ website/myWebsite/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^get/(?P<article_id>\d+)/$', views.article, name='articles'),
-    url(r'^hello/$', views.hello, name='hello'),
-    url(r'^hello_template/$', views.hello_template, name='hello_template'),
+    url(r'^user/(?P<uid>\d+)/$', views.user, name='user'),
+    url(r'^get/(?P<article_id>\d+)/$', views.article, name='article'),
+    url(r'^all', views.articles, name='articles'),
 ]

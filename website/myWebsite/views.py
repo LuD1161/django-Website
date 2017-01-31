89e22838beb66c386747c4a183ee1ec8ac0a508b
LuD1161 <aseemshrey@gmail.com>
2017-01-31 16:41:36 +0530

    added notifications , connect ,  friend request sending and friend searching methods

--- website/myWebsite/views.py
+++ website/myWebsite/views.py
@@ -1,5 +1,7 @@
+import json
 import logging
 
+from datetime import datetime
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, render
@@ -7,7 +9,8 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.template.loader import get_template
 from django.template import Context
-from .models import Articles, Users
+
+from .models import Articles, Users, Requests
 from .forms import RegistrationForm, TestForm
 from django.contrib.auth import authenticate, login as django_login, logout
 from django.contrib.auth.models import User
@@ -44,6 +47,7 @@ def language(request, lang='en-in'):
     return response
 
 
+@login_required
 def user(request, uid=1):
     return render_to_response('base.html',
                               {'user': Users.objects.get(user_id=uid)})
@@ -76,9 +80,71 @@ def login(request):
     return render(request, 'login.html', {'form': form})
 
 
+@login_required
 def logout_view(request):
     logout(request)
     return render_to_response('login.html')
+
+
+@login_required
+def findfriends(request):
+    if request.method == 'POST':
+        profile = Users.objects.get(user_email=request.user.email)
+        query = request.POST.get('q')
+        friends = Users.objects.filter(user_name__icontains=query)
+        dictionaries = [obj.as_dict() for obj in friends]
+
+        req = Requests.objects.filter(to_f=profile.user_id)
+        dictionary = [obj.as_dict() for obj in req]
+        data = [dictionaries] + [dictionary]
+        return HttpResponse(json.dumps(data), content_type='text/html')
+    else:
+        return HttpResponse('')
+
+
+@login_required
+def frequest(request):
+    if request.method == 'POST':
+        friend_id = request.POST.get('friend_1')
+        profile = Users.objects.get(user_email=request.user.email)
+        friend_name = Users.objects.get(user_id=friend_id).user_name
+        friend_req = Requests(to_f=profile.user_id, from_f=friend_id, uid=friend_id, status=0,
+                              date_sent=datetime.now(), from_user=friend_name)
+        friend_req.save()
+        return HttpResponse('ok')
+    else:
+        return HttpResponse('')
+
+
+@login_required
+def notifications(request):
+    notifs = []
+    profile = Users.objects.get(user_email=request.user.email)
+    logger.info(request.user.email)
+    req = Requests.objects.filter(to_f=profile.user_id, status=0)
+    for name in req:
+        user_pic = Users.objects.get(user_id=name.from_f).user_pic
+        dict = {"from_f": name.from_f, "from_user": name.from_user,
+                "from_user_pic": user_pic}
+        notifs.append(dict)
+    logger.info(notifs)
+    return HttpResponse(json.dumps(notifs), content_type='text/html')
+
+
+@login_required
+def connect(request, method, user_id):
+    result = ''
+    profile = Users.objects.get(user_email=request.user.email)
+    if method == 'accept':
+        result = Requests.objects.filter(to_f=profile.user_id, from_f=user_id).update(status=1)
+    elif method == 'block':
+        result = Requests.objects.filter(to_f=profile.user_id, from_f=user_id).update(status=2)
+    elif method == 'decline':
+        result = Requests.objects.filter(to_f=profile.user_id, from_f=user_id).update(status=3)
+    elif method == 'hide':
+        result = Requests.objects.filter(to_f=profile.user_id, from_f=user_id).update(status=4)
+
+    return HttpResponse(result)
 
 
 # Not needed

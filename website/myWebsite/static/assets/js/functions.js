2629dc3f156e18d1e4425944be0df59f19256918
LuD1161 <aseemshrey@gmail.com>
2017-02-01 22:52:32 +0530

    Respond to friend request function created , even though sill facing problems regarding dropdown of accept/decline

--- website/myWebsite/static/assets/js/functions.js
+++ website/myWebsite/static/assets/js/functions.js
@@ -12,29 +12,43 @@ function closeNav() {
     document.body.style.backgroundColor = "white";
 }
 
-//function sendReq(friend_1){
-//    //   console.log(friend_1);
-//      var data = "friend_1="+friend_1;
-//      var xhr = $.ajax({
-//      type:'POST',
-//      url:'../core/connect.php',
-//      data: data,
-//      complete: function(){
-//      //  console.log(this.url+" data == "+data);
-//      },
-//      success: function(data){
-//          console.log(data);
-//      if(data == 'ok'){
-//          $("#friendModalTitle").text("Connection Request Sent");
-//          $("#myFriendModal").modal({
-//            keyboard: true,
-//            show:     true
-//          });
-//          setInterval(function(){
-//              $("#myFriendModal").modal("hide");
-//          }, 1200);
-//          $("button[value='"+friend_1+"'] span").text("Request Sent");
-//      }
-//      }
-//      });
-//}
+function sendReq(friend_1){
+      var csrftoken = getCookie('csrftoken');
+      var data = "friend_1="+friend_1;
+      var xhr = $.ajax({
+      type:'POST',
+      headers: {'X-CSRFToken': csrftoken},
+      url:'/myWebsite/frequest',
+      data: data,
+      complete: function(){
+      },
+      success: function(data){
+      if(data == 'ok'){
+          $("#friendModalTitle").text("Connection Request Sent");
+          $("#myFriendModal").modal({
+            keyboard: true,
+            show:     true
+          });
+          setInterval(function(){
+              $("#myFriendModal").modal("hide");
+          }, 1200);
+          $("button[value='"+friend_1+"'] span").text("Request Sent");
+      }
+      }
+      });
+}
+
+function respond(link){
+    var csrftoken = getCookie('csrftoken');
+    var xhr = $.ajax({
+    type:'POST',
+    headers:{'X-CSRFToken': csrftoken},
+    url: link,
+    complete:function(){
+    },
+    success:function(data){
+    console.log("respond : "+data)
+    }
+    });
+}
+

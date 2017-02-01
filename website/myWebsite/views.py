55b98819288fd50b92615b0b3d9e2cfd06e888b7
LuD1161 <aseemshrey@gmail.com>
2017-02-01 17:51:20 +0530

    added messaging option and messages in notifications also

--- website/myWebsite/views.py
+++ website/myWebsite/views.py
@@ -13,7 +13,7 @@ from django.core.files.storage import FileSystemStorage
 from django.views.decorators.csrf import csrf_exempt
 
 from django.conf import settings
-from .models import Users, Requests
+from .models import Users, Requests, Messages
 from .forms import RegistrationForm, TestForm
 from django.contrib.auth import authenticate, login as django_login, logout
 from django.contrib.auth.models import User
@@ -27,25 +27,25 @@ logger.setLevel(logging.INFO)
 def uploadUserPic(request):
     if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
-        loc = settings.STATIC_ROOT+"/images/"
+        loc = settings.STATIC_ROOT + "/images/"
         fs = FileSystemStorage(location=loc, base_url=loc)
         filename = fs.save(myfile.name, myfile)
         # uploaded_file_url = fs.url(filename)
-        Users.objects.filter(user_email=request.user.email).update(user_pic=""+filename)
+        Users.objects.filter(user_email=request.user.email).update(user_pic="" + filename)
         return HttpResponseRedirect(request.META['HTTP_REFERER'])
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
 
 @login_required
 def user(request, uid=1):
-    profile = getProfile(request.user.email)
-    return render_to_response('base.html',
+    profile = getProfile(email=request.user.email)
+    return render_to_response('visitor.html',
                               {'user': profile, 'visitor': Users.objects.get(user_id=uid)})
 
 
 @login_required
 def accountsettings(request):
-    profile = getProfile(request.user.email)
+    profile = getProfile(email=request.user.email)
     return render_to_response('accountsettings.html',
                               {'user': profile})
 
@@ -71,7 +71,7 @@ def login(request):
 @login_required
 def logout_view(request):
     logout(request)
-    return render_to_response('login.html')
+    return HttpResponseRedirect('/')
 
 
 @login_required
@@ -94,7 +94,7 @@ def findfriends(request):
 def frequest(request):
     if request.method == 'POST':
         friend_id = request.POST.get('friend_1')
-        profile = getProfile(request.user.email)
+        profile = getProfile(email=request.user.email)
         friend_name = Users.objects.get(user_id=friend_id).user_name
         friend_req = Requests(to_f=profile.user_id, from_f=friend_id, uid=friend_id, status=0,
                               date_sent=datetime.now(), from_user=friend_name)
@@ -107,22 +107,32 @@ def frequest(request):
 @login_required
 def notifications(request):
     notifs = []
-    profile = getProfile(request.user.email)
-    logger.info(request.user.email)
+    profile = getProfile(email=request.user.email)
     req = Requests.objects.filter(to_f=profile.user_id, status=0)
+    request = []
     for name in req:
         user_pic = Users.objects.get(user_id=name.from_f).user_pic
-        dict = {"from_f": name.from_f, "from_user": name.from_user,
-                "from_user_pic": user_pic}
-        notifs.append(dict)
+        udict = {"from_f": name.from_f, "from_user": name.from_user,
+                 "from_user_pic": user_pic}
+        request.append(udict)
+    msgs = Messages.objects.filter(to_f=profile.user_id, status='unread')
+    msg = []
+    for message in msgs:
+        user_pic = Users.objects.get(user_id=message.from_f).user_pic
+        mdict = {"message_id": message.message_id, "from_f": message.from_f,
+                 "from_user": message.from_user, "message": message.message,
+                 "from_user_pic": user_pic}
+        msg.append(mdict)
     logger.info(notifs)
+    notifs.append(request)
+    notifs.append(msg)
     return HttpResponse(json.dumps(notifs), content_type='text/html')
 
 
 @login_required
 def connect(request, method, user_id):
     result = ''
-    profile = getProfile(request.user.email)
+    profile = getProfile(email=request.user.email)
     if method == 'accept':
         result = Requests.objects.filter(to_f=profile.user_id, from_f=user_id).update(status=1)
     elif method == 'block':
@@ -135,19 +145,42 @@ def connect(request, method, user_id):
     return HttpResponse(result)
 
 
+@login_required
 def Connections(request):
     data = []
-    profile = getProfile(request.user.email)
+    profile = getProfile(email=request.user.email)
     friends = Requests.objects.filter(to_f=profile.user_id, status=1)
     for friend in friends:
         q = Users.objects.extra(where=['user_id=%s'], params=[str(friend.from_f)])
         data.append(q.only("user_id", "user_fname", "user_lname", "user_pic", "user_about"))
-    return render(request, 'base.html', {'friends': data, 'user': profile})
+    return render(request, 'connections.html', {'friends': data, 'user': profile})
+
+
+@login_required
+@csrf_exempt
+def messages(request):
+    if request.method == 'POST':
+        if request.POST['recipient']:
+            message = request.POST.get('message')
+            recipient = request.POST.get('recipient')
+            profile = getProfile(email=request.user.email)
+            msg = Messages(to_f=recipient, from_f=profile.user_id,
+                           from_user=profile.user_fname + " " + profile.user_lname,
+                           status='unread', message=message, date_sent=datetime.now())
+            msg.save()
+            return HttpResponseRedirect(request.META['HTTP_REFERER'])
+        elif request.POST['read']:
+            return HttpResponseRedirect(request.META['HTTP_REFERER'])
+    else:
+        return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
 
 # Auxiliary Functions
-def getProfile(email):
-    return Users.objects.get(user_email=email)
+def getProfile(email='', uid=''):
+    if uid == '':
+        return Users.objects.get(user_email=email)
+    else:
+        return Users.objects.get(user_id=uid)
 
 
 # Not needed
